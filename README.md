d7967feeb46610c1f186308c54d35a38dd6fe7f9
Stephen Keep <stephen.keep@redant.com>
2016-01-22 15:55:28 +0000

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mandrill for NodeRED
+
+An simple node that sends and email using Mailchimps [Mandrill](https://mandrillapp.com/).
+
+## Installation
+
+`npm install node-red-contrib-mandrill`
+
+## Quick Start
+
+You will need valid Mandrill API key.  You can get these [here](https://mandrillapp.com/). 
+
+## Implemented Node
+
+ * Send Email - [https://mandrillapp.com/api/docs/](https://mandrillapp.com/api/docs/)
---
+++ mandrill-send.js
@@ -0,0 +1,44 @@
+var mandrill = require('mandrill-send')
+
+var _internals = {};
+
+_internals.send = function (payload, cb) {
+    
+   var email = mandrill(payload.api_key);
+
+   email(payload, function(err){
+      cb(err)
+    });
+};
+
+
+module.exports = function(RED) {
+    'use strict';
+
+    function Node(n) {
+      
+        RED.nodes.createNode(this,n);
+
+        var node = this;
+        
+
+        this.on('input', function (msg) {
+            
+            var payload = typeof msg.payload === 'object' ? msg.payload : {};
+        
+            var attrs = ['api_key','from','to','subject','html'];
+            for (var attr of attrs) {
+                if (n[attr]) {
+                    payload[attr] = n[attr];     
+                }
+            }
+
+            _internals.send(payload, function(err, result){
+
+                node.log(err);
+            });
+        });
+    }
+
+    RED.nodes.registerType('mandrill-send', Node);
+};
