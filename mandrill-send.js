49c3a24d0740072c38155e3f7a7877fc9abd6bd8
Ronit Kadwane <ronit@jumpbyte.com>
2017-11-01 18:42:39 +0530

    email with template and without template

--- mandrill-send.js
+++ mandrill-send.js
@@ -1,39 +1,57 @@
-var mandrill = require('mandrill-send')
+var mandrill = require('mandrill-api/mandrill')
 
 var _internals = {};
 
 _internals.send = function (payload, cb) {
-    
-   var email = mandrill(payload.api_key);
+    const mandrillClient = new mandrill.Mandrill(payload.api_key);
 
-   email(payload, function(err){
-      cb(err)
-    });
+    if (payload.template_name) {
+        mandrillClient.messages.sendTemplate(payload, function (result) {
+            cb(null, result)
+        },
+            function (error) {
+                cb(error)
+            }
+        );
+    } else {
+        mandrillClient.messages.send(payload, function (result) {
+            cb(null, result)
+        },
+            function (error) {
+                cb(error)
+            }
+        );
+    }
 };
 
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     'use strict';
 
     function Node(n) {
-      
-        RED.nodes.createNode(this,n);
+
+        RED.nodes.createNode(this, n);
 
         var node = this;
+        var payload = {};
         
-
+        
         this.on('input', function (msg) {
-            
-            var payload = typeof msg.payload === 'object' ? msg.payload : {};
-        
-            var attrs = ['api_key','from','to','subject','html'];
-            for (var attr of attrs) {
-                if (n[attr]) {
-                    payload[attr] = n[attr];     
-                }
+            if(typeof msg.payload === 'object'){
+                payload = msg.payload;
+                payload.api_key = payload.api_key ? payload.api_key : n.api_key;
+                
+                var message = payload.message;
+                message.to = message.to ? message.to : [{email: n.to}];
+                message.from_email = message.from_email ? message.from_email : n.from;
+                message.subject = message.subject ? message.subject :n.subject;
+                message.text = message.text ? message.text :n.text;
+
+                payload.template_name = payload.template_name ? payload.template_name : n.template_name;
+                payload.template_content = payload.template_content ? payload.template_content : [JSON.parse(n.template_content)] 
             }
 
-            _internals.send(payload, function(err, result){
+            _internals.send(payload, function (err, result) {
                 if (err) {
                     node.error(err, msg);
                 } else {
